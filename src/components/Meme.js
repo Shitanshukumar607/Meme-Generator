1e364c193f05cb031c8c4618af8cbd1c83f4b44d
Shitanshu <shitanshukumar639@gmail.com>
2024-06-29 19:41:44 +0530

    Updated the meme component

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,10 +1,27 @@
 export default function Meme() {
   return (
     <main>
-      <form>
-        <input type="text" placeholder="Top Text" />
-        <input type="text" placeholder="Bottom Text" />
-        <button>Get a new meme image 🖼</button>
+      <form className="form">
+        <div>
+          <label htmlFor="top-text">Top Text</label>
+          <input
+            id="top-text"
+            type="text"
+            placeholder="Shut up"
+            className="form--input"
+          />
+        </div>
+        <div>
+          <label>
+            Bottom Text
+            <input
+              type="text"
+              placeholder="and take my money"
+              className="form--input"
+            />
+          </label>
+        </div>
+        <button className="form--button">Get a new meme image 🖼</button>
       </form>
     </main>
   );
